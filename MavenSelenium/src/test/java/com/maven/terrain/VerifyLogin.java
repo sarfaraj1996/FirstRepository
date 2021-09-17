60fe1303a4002fe9c3815a0354dd95e8fccb4b9f
Sarfaraj Ahmed <sarfaraja@maveric-systems.com>
2021-09-17 13:26:28 +0530

    My First commit

---
+++ MavenSelenium/src/test/java/com/maven/terrain/VerifyLogin.java
@@ -0,0 +1,48 @@
+package com.maven.terrain;
+
+/**
+* 
+*/
+
+ 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+ 
+import com.maven.terrain.Login ;
+ 
+/**
+* @author Mukesh_50
+*
+*/
+public class VerifyLogin 
+{
+ 
+ 
+@Test
+public void verifyValidLogin() throws InterruptedException
+{
+System.setProperty("webdriver.gecko.driver","C:\\Users\\sarfaraja\\eclipse-SeleniumProject\\seleniumProject\\drivers\\geckodriver.exe");
+WebDriver driver=new FirefoxDriver();
+ 
+driver.manage().window().maximize();
+ 
+driver.get("https://opensource-demo.orangehrmlive.com/");
+ 
+Login login=new Login(driver);
+ 
+Thread.sleep(3000);
+login.loginToWordpress("Admin","admin123");
+Thread.sleep(5000);
+//login.typePassword("admin123");
+//Thread.sleep(3000);
+//login.clickOnLoginButton();
+//Thread.sleep(3000);
+ 
+ 
+driver.quit();
+ 
+}
+ 
+ 
+}
